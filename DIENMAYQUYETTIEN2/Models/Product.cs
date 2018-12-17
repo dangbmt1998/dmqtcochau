1eef5a660cca95fae93f2017de51e04a7a57ca75
Thanh Dang <dangbmt1998@gmail.com>
2018-12-17 15:44:52 +0700

    image create

--- DIENMAYQUYETTIEN2/Models/Product.cs
+++ DIENMAYQUYETTIEN2/Models/Product.cs
@@ -20,30 +20,30 @@ namespace DIENMAYQUYETTIEN2.Models
             this.InstallmentBillDetails = new HashSet<InstallmentBillDetail>();
         }
         public int ID { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập mã sản phẩm")]
-        [StringLength(10, MinimumLength = 5, ErrorMessage = "Mã sản phẩm không được nhiều hơn 10 ký tự")]
+       // [Required(ErrorMessage = "Vui lòng nhập mã sản phẩm")]
+        //[StringLength(10, MinimumLength = 5, ErrorMessage = "Mã sản phẩm không được nhiều hơn 10 ký tự")]
         public string ProductCode { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
-       [StringLength(100, MinimumLength = 3, ErrorMessage = "Tên sản phẩm không được nhiều hơn 100 ký tự")]
+        //[Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
+       //[StringLength(100, MinimumLength = 3, ErrorMessage = "Tên sản phẩm không được nhiều hơn 100 ký tự")]
         public string ProductName { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập Mã sản phẩm")]
+        //[Required(ErrorMessage = "Vui lòng nhập Mã sản phẩm")]
         public int ProductTypeID { get; set; }
-               [Required(ErrorMessage = "Vui lòng nhập giá bán")]
-         [DataType(DataType.Currency)]
+         //      [Required(ErrorMessage = "Vui lòng nhập giá bán")]
+         //[DataType(DataType.Currency)]
         public int SalePrice { get; set; }
-               [Required(ErrorMessage = "Vui lòng nhập giá gốc")]
-          [DataType(DataType.Currency)]
+           //    [Required(ErrorMessage = "Vui lòng nhập giá gốc")]
+          //[DataType(DataType.Currency)]
          public int OriginPrice { get; set; }
-          [Required(ErrorMessage = "Vui lòng nhập giá trả góp")]
-          [DataType(DataType.Currency)]
+         // [Required(ErrorMessage = "Vui lòng nhập giá trả góp")]
+         // [DataType(DataType.Currency)]
         public int InstallmentPrice { get; set; }
-          [Range(1, 100, ErrorMessage = "Số lượng không quá 100 món")]
-         [Required(ErrorMessage = "Vui lòng nhập số lượng")]
+         // [Range(1, 100, ErrorMessage = "Số lượng không quá 100 món")]
+         //[Required(ErrorMessage = "Vui lòng nhập số lượng")]
         public int Quantity { get; set; }
 
         public string Avatar { get; set; }
 
-         [Required(ErrorMessage = "Vui lòng nhập trạng thái")]
+         //[Required(ErrorMessage = "Vui lòng nhập trạng thái")]
         public Nullable<bool> Status { get; set; }
         public virtual ICollection<CashBillDetail> CashBillDetails { get; set; }
         public virtual ICollection<InstallmentBillDetail> InstallmentBillDetails { get; set; }
